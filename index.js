d1e36e3f4918fcfd17bfac4b0ef11e282067f0f3
afeiship <1290657123@qq.com>
2019-06-30 23:08:51 +0800

    add: done

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ const token = fs
   .trim();
 const cookie = '1&_token=' + token + ';';
 
+// fetch('https://www.ximalaya.com/revision/main/getCurrentUser', {
+//   headers: {
+//     cookie
+//   }
+// })
+//   .then((res) => res.json())
+//   .then((res) => {
+//     console.log(res);
+//   });
 
-fetch('https://www.ximalaya.com/revision/main/getCurrentUser', {
+fetch('https://www.ximalaya.com/revision/comment/like', {
+  method: 'POST',
+  body: JSON.stringify({ trackId: 194513320, commentId: 119944909 }),
   headers: {
+    Referer: 'https://www.ximalaya.com/qinggan/261139/194513320',
+    'Content-Type': 'application/json',
     cookie
   }
 })
   .then((res) => res.json())
   .then((res) => {
-    console.log(res);
+    console.log('res:->', res);
   });
